da347c93cceeef7ae3f2a5cc5bedbbcf120426ca
Taylor Harwin <taylor.harwin@gmail.com>
2014-03-20 17:00:03 -0700

    Almost working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,57 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
+//Recursive stringify function
+
 var stringifyJSON = function (obj) {
-  // your code goes here
+var Counter = '';
+  
+    //case where nothing's provided
+    if (!arguments.length) {
+        return Counter;
+    }
+  
+    //case for strings
+    else if (typeof obj === 'string') {
+        Counter += "\"" + obj + "\"";
+    }
+  //case for null
+  else if (obj === null){
+    Counter += 'null';
+  }
+  
+    //case for numbers, booleans, functions, and undefined
+  else if (typeof obj === 'number' || typeof obj === 'boolean'){
+        Counter += obj.toString();
+    }
+  else if (typeof obj === "function" || obj === undefined){
+    Counter += obj.toString();
+  }
+    //case for arrays
+    else if (Array.isArray(obj)) {
+        Counter += '[';
+        _.each(obj, function (element, index) {
+            if (index > 0 && index !== obj.length) {
+                Counter += ',';
+            }
+            Counter += stringifyJSON(element);
+        });
+        Counter += ']';
+    }
+
+
+    //case for objects
+    else {
+        var commacount = 0;
+        Counter += '{';
+        _.each(obj, function (value, key) {
+            if (commacount > 0) {
+                Counter += ',';
+            }
+            Counter += stringifyJSON(key) + ':' + stringifyJSON(value);
+            commacount++;
+        });
+        Counter += "}";
+    }
+    return Counter;
 };
