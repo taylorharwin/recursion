521ba64cd9d4327d7c281c526e001c2a84e6fd28
Hack Reactor Students <taylor.harwin@gmail.com>
2014-04-14 16:41:03 -0700

    Working on objects still

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,59 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+
+  //'9'
+  if (json.match(/[0-9]/)){
+
+    return parseInt(json);
+
+  }
+  if (json === 'true'){
+    return true;
+  }
+  else if (json === 'false'){
+    return false;
+  }
+
+  if (json[0] === '['){
+
+    //json.replace(/[\[\]]/g,'');
+    json = json.substring(1,json.length - 1);
+    var str = '';
+    var arr = json.join(',');
+
+
+    for (var i = 0; i < arr.length; i++){
+
+      if (i === 0){
+
+        if (Array.isArray(arr[0])){
+
+          str = str + parseJSON(arr[0]);
+        }else{
+
+          str = str + arr[0];
+        }
+
+      }else{
+
+        if (Array.isArray(arr[i])){
+
+          str = str + ',' + parseJSON(arr[i]);
+        }else{
+
+          str = str + ',' + arr[i];
+        }
+      }
+    }
+
+      return str.join(',');
+   }
+
+   if (json[0] === '{'){
+
+
+   }
+
+
 };
